600ef8842c42d926857d31b278b51bb9ff21b6dd
lorisclivaz <lorisclivaz@hotmail.com>
2020-10-09 16:20:18 +0200

    init commit

---
+++ lib/Models/UserpersonalData.dart
@@ -0,0 +1,20 @@
+
+class UserPersonalData
+{
+
+  //Variables personalData
+  String uid;
+  String email;
+  String nom;
+  String prenom;
+
+  //Constructeur
+  userPersonalData(String uid, String email, String nom, String prenom){
+
+    this.uid = uid;
+    this.email = email;
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,273 @@
+import 'package:cercle_project/Home/HomePage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:cercle_project/authentication/SignUp.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+    // Title
+      title: "Simple Material App",
+      // Home
+      home: LoginScreen()));
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Remember me
+  bool _rememberMe = false;
+
+  //Variable saisie utilisateur
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+
+  Widget _buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Email',
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          child: TextField(
+            controller: emailController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)
+              ) ,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Email',
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Password',
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          child: TextField(
+            controller: password,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+              ),
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Password',
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildForgotPassword() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: FlatButton(
+        onPressed: () => print('Forgot password button pressed'),
+        padding: EdgeInsets.only(right: 0.0),
+        child: Text('Forgot Password'),
+      ),
+    );
+  }
+
+  Widget _buildRememberMeCheckBox(){
+    return  Container(
+      height: 20.0,
+      child: Row(
+        children: [
+          Theme(
+            data:
+            ThemeData(unselectedWidgetColor: Colors.white),
+            child: Checkbox(
+              value: _rememberMe,
+              checkColor: Colors.green,
+              activeColor: Colors.white,
+              onChanged: (value){
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+            ),
+          ),
+          Text('Remember me')
+        ],
+      ),
+    );
+  }
+
+  Widget _builLoginButton(){
+    return   Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () { Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage() ),
+
+        );
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.white,
+        child: Text('LOGIN',
+          style: TextStyle(
+              color: Color(0xFF527DAA),
+              letterSpacing: 1.5,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'OpenSans'
+          ),),
+      ),
+
+    );
+  }
+
+  Widget _buildSignUp(){
+    return  GestureDetector(
+      onTap:() {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SignUp() ),
+
+        );
+      },
+      child: RichText(text: TextSpan(
+        children: [
+        TextSpan(
+        text: 'Don\'t have an Account',
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+        TextSpan(
+          text: 'Sign Up',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose signIn page');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => Focus.of(context).unfocus(),
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF73AEF5),
+                    Color(0xFF61A4F1),
+                    Color(0xFF478DE0),
+                    Color(0xFF398AE5),
+                  ],
+                  stops: [0.1, 0.4, 0.7, 0.9],
+                )),
+              ),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 120.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      _buildEmail(),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      _buildPassword(),
+                      _buildForgotPassword(),
+                      _buildRememberMeCheckBox(),
+                      _builLoginButton(),
+                      _buildSignUp(),
+
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
